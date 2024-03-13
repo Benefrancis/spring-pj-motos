026240e1934879b86952a9463fbeaa7d51ba5780
Cristian <cristianalvaro0504@gmail.com>
2024-03-13 00:07:42 -0300

    Corrigindo o Simulado

---
+++ src/main/java/br/com/fiap/springpjmotos/resources/LojaResource.java
@@ -0,0 +1,36 @@
+package br.com.fiap.springpjmotos.resources;
+
+import br.com.fiap.springpjmotos.entity.Acessorio;
+import br.com.fiap.springpjmotos.entity.Loja;
+import br.com.fiap.springpjmotos.repository.AcessorioRepository;
+import br.com.fiap.springpjmotos.repository.LojaRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/loja")
+public class LojaResource {
+
+    @Autowired
+    private LojaRepository repo;
+
+    @GetMapping
+    public List<Loja> findAll(){
+        return repo.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Loja findById(@PathVariable Long id){
+        return repo.findById(id).orElseThrow();
+    }
+
+    @Transactional
+    @PostMapping
+    public Loja save(@RequestBody Loja loja){
+        return repo.save(loja);
+    }
+
+}
